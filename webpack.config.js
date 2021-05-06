ad7d63be7abb21a9d26744481b2ab56ce4c1425a
Steve Larsen <slarsen-3@att.net>
2021-05-06 14:49:17 -0500

    cach busting and HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "[name].[contentHash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/template.html"
+  })],
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ["style-loader", // 3. Inject styles into DOM
-         "css-loader", // 2. Turns css into common js
-          "sass-loader" // 1. Turns sass into css
+        use: [
+          "style-loader", // 3. Inject styles into DOM
+          "css-loader", // 2. Turns css into common js
+          "sass-loader", // 1. Turns sass into css
         ],
       },
     ],
