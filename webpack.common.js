af65c9c04f74363eb46a165513538332874c909e
Steve Larsen <slarsen-3@att.net>
2021-05-07 18:30:22 -0500

    split webpack-prod and webpack-dev

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,12 @@
 const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "[name].[contentHash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [new HtmlWebpackPlugin({
-    template: "./src/template.html"
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
     rules: [
       {
